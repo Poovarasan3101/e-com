635155c98af10b682781e81f2e7bb2535fcac908
poovarasan <slorowdyy@gmail.com>
2026-01-02 06:58:25 +0000

    i initial commit :wq

---
+++ script.js
@@ -0,0 +1,9 @@
+var sidenavbar=document.querySelector(".side-navbar")
+    function men(){
+ sidenavbar.style.left="0%"
+ 
+}
+
+var sidnn=document.querySelector(".side-navbar")
+    function closenn()
+    {sidnn.style.left="-60%"}
